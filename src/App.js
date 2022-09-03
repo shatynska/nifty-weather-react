f89bb070d0ad03aa319e446acd16b9b9cccd9b9f
Helen Shatynska <94306487+niftywebsite@users.noreply.github.com>
2022-09-03 15:31:39 +0300

    added github and website links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        <a
+          href="https://github.com/niftywebsite/nifty-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://getaniftywebsite.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Helen Shatynska
+        </a>
+      </p>
     </div>
   );
 }
